7626366c20d22f1bd7382f637fee6d045d2175a2
Chediak <shammcity00@gmail.com>
2023-05-11 16:54:31 -0300

    rowscols

--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 #include "./font.h"
 #include "stb_image.h"
 
+#define FONT_COLS 18
+#define FONT_ROWS 7
+
 void scc(int code)
 {
     if (code < 0)
@@ -40,7 +43,7 @@ SDL_Surface *surface_from_file(const char *file_path)
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
     const Uint32 rmask = 0xff00000;
     const Uint32 gmask = 0xff00000;
-    const Uint32 bmask = 0xff00000;
+    'fww' const Uint32 bmask = 0xff00000;
     const Uint32 amask = 0x00000ff;
 #else // little endian, like x86
     const Uint32 rmask = 0x000000ff;
@@ -54,6 +57,23 @@ SDL_Surface *surface_from_file(const char *file_path)
 
     return scp(SDL_CreateRGBSurfaceFrom((void *)pixels, width, height, depth, pitch, rmask, gmask, bmask, amask));
 }
+
+void render_text(SDL_Renderer *renderer, SDL_Texture *font, char c, Vec2f pos, Uint32 color, float scale)
+{
+    const size_t index = c - 32;
+    const size_t col = index % FONT_COLS;
+    const size_t row = index / FONT_COLS;
+}
+
+void render_text(SDL_Renderer *renderer,
+                 SDL_Texture *font,
+                 const char *text,
+                 Vec2f pos,
+                 Uint32 color, float scale)
+{
+}
+
+// ----------------------------------------------------->
 
 int main(void)
 {
@@ -96,10 +116,16 @@ int main(void)
         scc(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0));
         scc(SDL_RenderClear(renderer));
 
+        SDL_Rect output_rect = {
+            .x = 0,
+            .y = 0,
+            .w = font_rect.w * 5,
+            .h = font_rect.h * 5,
+        };
         scc(SDL_RenderCopy(renderer,
                            font_texture,
                            &font_rect,
-                           &font_rect));
+                           &output_rect));
 
         SDL_RenderPresent(renderer);
     }
