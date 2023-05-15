ad4a65921852f7aba0c339c6656b9016f4c5d2a6
Chediak <shammcity00@gmail.com>
2023-05-15 11:47:52 -0300

    post

--- main.c
+++ main.c
@@ -6,9 +6,14 @@
 #include <SDL2/SDL.h>
 #include "./font.h"
 #include "stb_image.h"
+#include "./la.h"
 
+#define FONT_WIDTH 128
+#define FONT_HEIGHT 64
 #define FONT_COLS 18
 #define FONT_ROWS 7
+#define FONT_CHAR_WIDTH (FONT_WIDTH / FONT_COLS)
+#define FONT_CHAR_HEIGHT (FONT_HEIGHT / FONT_ROWS)
 
 void scc(int code)
 {
@@ -63,6 +68,22 @@ void render_text(SDL_Renderer *renderer, SDL_Texture *font, char c, Vec2f pos, U
     const size_t index = c - 32;
     const size_t col = index % FONT_COLS;
     const size_t row = index / FONT_COLS;
+
+    const SDL_Rect src = {
+        .x = col * FONT_CHAR_WIDTH,
+        .y = row * FONT_CHAR_HEIGHT,
+        .w = FONT_CHAR_WIDTH,
+        .h = FONT_CHAR_HEIGHT,
+    };
+
+    const SDL_Rect dst = {
+        .x = (int)floorf(pos.x),
+        .y = (int)floorf(pos.y),
+        .w = (int)floorf(FONT_CHAR_WIDTH * scale),
+        .h = (int)floorf(FONT_CHAR_HEIGHT * scale),
+    };
+
+    scc(SDL_RenderCopy(renderer, font, &src, &dst));
 }
 
 void render_text(SDL_Renderer *renderer,
@@ -71,9 +92,13 @@ void render_text(SDL_Renderer *renderer,
                  Vec2f pos,
                  Uint32 color, float scale)
 {
+    size_t n = strlen(text);
+    Vec2f pen = pos;
+    for (size_t i = 0; i < n; ++i)
+    {
+        render_text(renderer, font, text[i], pen, color, scale);
+    }
 }
-
-// ----------------------------------------------------->
 
 int main(void)
 {
